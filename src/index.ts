7def4e6fd12ab9306c1d5447fa6612f306d65d43
AllusiveWheat <154700875+AllusiveWheat@users.noreply.github.com>
2024-12-11 16:28:14 -0800

    update port

--- src/index.ts
+++ src/index.ts
@@ -61,6 +61,6 @@ app.get("/server", async (req: Request, res: Response) => {
   res.json(app.locals.serverData);
 });
 
-app.listen(3000, () => {
-  console.log("Server started on port 3000");
+app.listen(3001, () => {
+  console.log("Server started on port 3001");
 });
